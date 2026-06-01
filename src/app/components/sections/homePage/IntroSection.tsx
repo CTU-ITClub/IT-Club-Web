277d1191b3b547719372c35d4a5fcf8a93b53ea1
Lam Sơn <hhe>
2026-06-02 00:47:52 +0700

    update UI of IntroSection

--- src/app/components/sections/homePage/IntroSection.tsx
+++ src/app/components/sections/homePage/IntroSection.tsx
@@ -21,9 +21,9 @@ const Items = [
 
 export function IntroSection({ isDark }: IntroSectionProps) {
   return (
-    <section className="py-20 md:py-28">
-      <div className="max-w-7xl mx-auto w-full">
-        <div className="py-10 md:py-18 flex justify-between flex-col lg:flex-row items-center gap-10">
+    <section className="py-20 md:py-28 ">
+      <div className="max-w-7xl mx-auto w-full  px-4 sm:px-6">
+        <div className="py-10 md:py-18 md:pb-8 flex justify-between flex-col lg:flex-row items-center gap-10">
           <div>
             <h2
               className={`text-3xl sm:text-4xl md:text-5xl font-extrabold mb-4 font-sans ${
@@ -34,13 +34,13 @@ export function IntroSection({ isDark }: IntroSectionProps) {
             </h2>
             <div className="space-y-4 mt-10 max-w-2xl md:max-w-xl text-lg sm:text-xl md:text-2xl">
               <p
-                className={`text-lg sm:text-xl md:text-2xl leading-relaxed max-w-3xl ${isDark ? "text-white/90" : "text-slate-700"}`}
+                className={`text-lg sm:text-lg md:text-xl leading-relaxed max-w-3xl ${isDark ? "text-white/90" : "text-slate-700"}`}
               >
                 Câu lạc bộ Tin học được thành lập với sứ mệnh tạo ra một cộng
                 đồng năng động cho những người đam mê công nghệ thông tin.
               </p>
               <p
-                className={`text-lg sm:text-xl md:text-2xl leading-relaxed max-w-3xl ${isDark ? "text-white/90" : "text-slate-700"}`}
+                className={`text-lg sm:text-lg md:text-xl leading-relaxed max-w-3xl ${isDark ? "text-white/90" : "text-slate-700"}`}
               >
                 Từ những buổi workshop thực hành, các dự án nhóm, các cuộc thi
                 CICT ICPC, CLB Tin học luôn mang đến những trải nghiệm học tập
@@ -48,14 +48,11 @@ export function IntroSection({ isDark }: IntroSectionProps) {
               </p>
             </div>
           </div>
-
-          <div className="rounded-2xl overflow-hidden shadow-lg group">
-            <img
-              className="w-full max-w-xl h-56 sm:h-64 md:h-92 lg:h-105 object-cover transition-transform duration-300 ease-out group-hover:scale-105 group-hover:-translate-y-1 transform-gpu"
-              src="https://scontent.fvca1-4.fna.fbcdn.net/v/t39.30808-6/657173409_1525823052697559_3491735791438855593_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFU15LjFtztxwgR1JkpSMgpJIv9Nkcu6gEki_02Ry7qASfpytdJ0XHXI07qezHW3jFhAs4Ay1X74LXojGz657uU&_nc_ohc=lA21II0idD8Q7kNvwEz1jnQ&_nc_oc=Adp_svQm0BhR5f158xTQKR_ldAa1mj08R2FBVFLiLeWgQGQpT5mACyuCTviv8pQL3kIt9blaf76cyc4WpsMQHTvs&_nc_zt=23&_nc_ht=scontent.fvca1-4.fna&_nc_gid=a__hopuEUEaLf6N0qUfUtw&_nc_ss=7b2a8&oh=00_Af_s_lvkecIG4XtkkM30tYnCueftzcZk_DmrRjnXPHugWQ&oe=6A218126"
-              alt="IT CLUB image"
-            />
-          </div>
+          <img
+            className="cursor-pointer rounded-2xl shadow-lg w-full max-w-xl h-56 sm:h-64 md:h-92 lg:h-105 object-cover transition-transform duration-400 ease-out hover:scale-105 hover:-translate-y-1 transform-gpu"
+            src="https://scontent.fvca1-4.fna.fbcdn.net/v/t39.30808-6/657173409_1525823052697559_3491735791438855593_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFU15LjFtztxwgR1JkpSMgpJIv9Nkcu6gEki_02Ry7qASfpytdJ0XHXI07qezHW3jFhAs4Ay1X74LXojGz657uU&_nc_ohc=lA21II0idD8Q7kNvwEz1jnQ&_nc_oc=Adp_svQm0BhR5f158xTQKR_ldAa1mj08R2FBVFLiLeWgQGQpT5mACyuCTviv8pQL3kIt9blaf76cyc4WpsMQHTvs&_nc_zt=23&_nc_ht=scontent.fvca1-4.fna&_nc_gid=a__hopuEUEaLf6N0qUfUtw&_nc_ss=7b2a8&oh=00_Af_s_lvkecIG4XtkkM30tYnCueftzcZk_DmrRjnXPHugWQ&oe=6A218126"
+            alt="IT CLUB image"
+          />
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-10">
@@ -70,7 +67,7 @@ export function IntroSection({ isDark }: IntroSectionProps) {
                     : "bg-white border border-blue-100/60 hover:border-blue-300"
                 }`}
               >
-                <div className="flex flex-col md:flex-row items-center gap-4 mb-4">
+                <div className="flex flex-col md:flex-row items-center gap-6 mb-4">
                   <div
                     className={`w-20 h-20 rounded-full flex items-center justify-center ${isDark ? "bg-cyan-400/10" : "bg-blue-50"}`}
                   >
@@ -79,7 +76,7 @@ export function IntroSection({ isDark }: IntroSectionProps) {
                     />
                   </div>
                   <h3
-                    className={`text-2xl sm:text-3xl font-bold font-sans mt-1 ${
+                    className={`text-2xl sm:text-3xl md:text-4xl font-extrabold font-sans ${
                       isDark ? "text-cyan-300" : "text-blue-800"
                     }`}
                   >
@@ -87,7 +84,7 @@ export function IntroSection({ isDark }: IntroSectionProps) {
                   </h3>
                 </div>
                 <p
-                  className={`text-base ${isDark ? "text-white/90" : "text-slate-700"}`}
+                  className={` sm:text-lg md:text-xl leading-relaxed font-sans ${isDark ? "text-white/90" : "text-slate-700"}`}
                 >
                   {t.description}
                 </p>
